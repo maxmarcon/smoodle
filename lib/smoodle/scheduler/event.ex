4540893be5ff3941618a9a20e3ecb5d16d3cc380
Max Marcon <maxmarcon@gmx.net>
2018-03-03 19:22:54 +0100

    added time window validations to events

--- lib/smoodle/scheduler/event.ex
+++ lib/smoodle/scheduler/event.ex
@@ -1,6 +1,7 @@
 defmodule Smoodle.Scheduler.Event do
   use Ecto.Schema
   alias Smoodle.Scheduler.Event
+  require Integer
   import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -19,9 +20,37 @@ defmodule Smoodle.Scheduler.Event do
   @doc false
   def changeset_create(%Event{} = event, attrs) do
     event
-    |> cast(attrs, [:name, :time_window_from, :time_window_to, :desc])
+    |> cast(attrs, [:name, :time_window_from, :time_window_to, :scheduled_from, :scheduled_to, :desc])
     |> validate_required([:name, :desc])
     |> validate_length(:name, max: 255, count: :codepoints)
     |> validate_length(:desc, max: 2500, count: :codepoints)
+    |> validate_time_window_defined([:time_window_from, :time_window_to], :time_window)
+    |> validate_time_window_defined([:scheduled_from, :scheduled_to], :scheduled)
+    |> validate_time_window_consistent([:time_window_from, :time_window_to], :time_window)
+    |> validate_time_window_consistent([:scheduled_from, :scheduled_to], :scheduled)
+  end
+
+
+  defp validate_time_window_defined(changeset, keys, error_key) do
+    if apply_changes(changeset)
+      |> Map.take(keys)
+      |> Map.values
+      |> Enum.count(&(is_nil(&1)))
+      |> Integer.is_odd
+    do
+      add_error(changeset, error_key, "both ends must be defined or none")
+    else
+      changeset
+    end
+  end
+
+  defp validate_time_window_consistent(changeset, keys, error_key) do
+    if Enum.all?(applied = (apply_changes(changeset) |> Map.take(keys) |> Map.values), &(!is_nil(&1)))
+      && Enum.reduce(applied, &(DateTime.diff(&1, &2))) <= 0
+    do
+      add_error(changeset, error_key, "the right side of the window must happen later than the left one")
+    else
+      changeset
+    end
   end
 end
--- test/smoodle/scheduler/event_test.exs
+++ test/smoodle/scheduler/event_test.exs
@@ -3,14 +3,21 @@ defmodule Smoodle.Scheduler.EventTest do
 
 	alias Smoodle.Scheduler.Event
 
-	@valid_attrs %{name: "Party", desc: "Yeah!"}
+	@valid_attrs %{
+		name: "Party",
+		desc: "Yeah!",
+		time_window_from: "2018-01-10 00:00:00+00",
+		time_window_to: "2018-03-20 00:00:00+00",
+		scheduled_from: "2018-02-05 19:00:00+00",
+		scheduled_to: "2018-02-05 23:00:00+00"
+	}
 
 	test "changeset with valid attrs" do 
 		changeset = Event.changeset_create(%Event{}, @valid_attrs)
 		assert changeset.valid?
 	end
 
-	test "changeset with without name" do 
+	test "changeset without name" do 
 		changeset = Event.changeset_create(%Event{}, Map.delete(@valid_attrs, :name))
 		assert %{name: ["can't be blank"]} = errors_on(changeset)
 	end
@@ -24,4 +31,25 @@ defmodule Smoodle.Scheduler.EventTest do
 		changeset = Event.changeset_create(%Event{}, Map.replace!(@valid_attrs, :desc, String.pad_trailing("Yeah!", 2501, "123")))
 		assert %{desc: ["should be at most 2500 character(s)"]} = errors_on(changeset)
 	end
+
+	test "validate both time window ends must be defined" do
+		changeset = Event.changeset_create(%Event{}, Map.delete(@valid_attrs, :time_window_from))
+		assert %{time_window: ["both ends must be defined or none"]} = errors_on(changeset)
+	end
+
+	test "validate both scheduled ends must be defined" do
+		changeset = Event.changeset_create(%Event{}, Map.delete(@valid_attrs, :scheduled_to))
+		assert %{scheduled: ["both ends must be defined or none"]} = errors_on(changeset)
+	end
+
+	test "validate both time window ends must be consistent" do
+		changeset = Event.changeset_create(%Event{}, Map.replace!(@valid_attrs, :time_window_from, "2018-03-25 00:00:00+00"))
+		assert %{time_window: ["the right side of the window must happen later than the left one"]} = errors_on(changeset)
+	end
+
+	test "validate both scheduled ends must be consistent" do
+		changeset = Event.changeset_create(%Event{}, Map.replace!(@valid_attrs, :scheduled_to, "2018-02-05 18:00:01+00"))
+		assert %{scheduled: ["the right side of the window must happen later than the left one"]} = errors_on(changeset)
+	end
+
 end
