55d052828a6691648f83b2de2a7421a00dbe73eb
Max Marcon <max.marcon@plugintheworld.com>
2018-06-15 16:59:29 +0200

    added index and integer key to polls

--- priv/repo/migrations/20180512221915_create_polls.exs
+++ priv/repo/migrations/20180512221915_create_polls.exs
@@ -2,13 +2,14 @@ defmodule Smoodle.Repo.Migrations.CreatePolls do
   use Ecto.Migration
 
   def change do
-    create table(:polls, primary_key: false) do
-      add :id, :uuid, primary_key: true
+    create table(:polls) do
       add :participant, :string, null: false
       add :preferences, :json
       add :event_id, references(:events, on_delete: :delete_all, on_update: :update_all, type: :uuid), null: false
 
       timestamps()
     end
+
+    #create unique_index(:polls, [:event_id, :participant])
   end
 end
