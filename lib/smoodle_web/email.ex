b6ab9be4ea4fb344b1c45c3f792fa7e6a74bc5f8
Max Marcon <maxmarcon@gmx.net>
2018-09-10 22:03:40 +0200

    better mail i18n

--- lib/smoodle_web/email.ex
+++ lib/smoodle_web/email.ex
@@ -2,12 +2,12 @@ defmodule SmoodleWeb.Email do
   use Bamboo.Phoenix, view: SmoodleWeb.EmailView
   import SmoodleWeb.Gettext
 
-  @from_address {gettext("Your LetsMeet! Bot"), "noreply@lets-meet.app"}
+  @from_address "noreply@lets-meet.app"
 
   def new_event_email(event) do
     new_email()
     |> to(event.email)
-    |> from(@from_address)
+    |> from({gettext("Your LetsMeet! Bot"), @from_address})
     |> subject(gettext("Your new event: %{event_name}", event_name: event.name))
     |> assign(:owner_link, SmoodleWeb.EventView.owner_link(event))
     |> assign(:share_link, SmoodleWeb.EventView.share_link(event))
