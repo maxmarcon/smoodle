c49dfe379f8dcb958225723adee64e717528754a
Max Marcon <maxmarcon@gmx.net>
2019-04-10 20:44:45 +0200

    pollEditor: replaced stale heart with thumb-up

--- assets/js/vue/pollEditor.vue
+++ assets/js/vue/pollEditor.vue
@@ -97,7 +97,7 @@
 							.form-group.row
 								.col.text-center.text-justify
 									i18n.small.text-muted(path="poll_editor.date_ranker_helper" tag="p")
-										i.fas.fa-heart.text-success(place="good")
+										i.fas.fa-thumbs-up.text-success(place="good")
 										i.fas.fa-thumbs-down.text-danger(place="bad")
 										i.fas.fa-circle.text-warning(place="indifferent")
 
