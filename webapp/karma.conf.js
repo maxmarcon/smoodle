e6ab2dbf5543fe93c64d675231a3f04eb047f042
Max Marcon <maxmarcon@gmx.net>
2019-11-13 19:06:10 +0100

    circleci: added front-end tests

--- webapp/karma.conf.js
+++ webapp/karma.conf.js
@@ -35,6 +35,10 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'kjhtml'],
 
+    junitReporter: {
+      outputDir: '../test-results/karma'
+    },
+
     // web server port
     port: 9876,
 
