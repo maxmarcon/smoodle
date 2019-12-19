a885cbc2b705674a2105200916e2cbbd7d314ae6
Max Marcon <maxmarcon@gmx.net>
2019-12-19 22:57:51 +0100

    updated home page

--- webapp/src/components/home.vue
+++ webapp/src/components/home.vue
@@ -1,80 +1,58 @@
 <template lang="pug">
-  div
-    .card
-      .card-header.text-center
-        h1.card-title.text-success
-          em {{ $t('app_name') }}
-        h5.card-title.text-muted {{ $t('home.title') }}
-      .card-body.text-center
-        .row.align-items-center
-          .col-md-6.mt-2
-            .p-3.border.border-success.rounded.card-text.text-muted
-              i18n(path="home.subtitle_1" tag="em")
-                span.text-success(place="app_name") {{ $t('app_name') }}
-          .col-md-6.mt-2
-            .p-3.border.border-success.rounded.card-text.text-muted
-              em {{ $t('home.subtitle_2') }}
-        .justify-content-center.mt-2
-          .p-3
-            h5.card-title.text-info {{ $t('home.how_it_works') }}
-        .card-deck
-          .card
-            .card-header
-              i18n.card-title.text-muted(path="home.step1.title" tag="h6")
-                span.h5.text-primary(place="step") {{ $t('home.step', {step: 1})}}
-            .card-body
-              ul.list-group.list-group-flush
-                li.list-group-item.text-muted
-                  em {{ $t('home.step1.part1') }}
-                li.list-group-item.text-muted
-                  em {{ $t('home.step1.part2') }}
-                li.list-group-item
-                  img.img-fluid.w-75.border.border-primary.rounded(src="../assets/event-creation-calendar.png")
-                li.list-group-item.text-muted
-                  em {{ $t('home.step1.part3') }}
-          .card
-            .card-header
-              i18n.card-title.text-muted(path="home.step2.title" tag="h6")
-                span.h5.text-primary(place="step") {{ $t('home.step', {step: 2})}}
-            .card-body
-              ul.list-group.list-group-flush
-                li.list-group-item.d-flex.justify-content-around.pb-4
-                  i.icon.fas.fa-thumbs-up.fa-2x.text-success
-                  i.icon.fas.fa-circle.fa-2x.text-warning
-                  i.icon.fas.fa-thumbs-down.fa-2x.text-danger
-                li.list-group-item.text-muted
-                  i18n(path="home.step2.part1" tag="em")
-                    span.text-success(place="good") {{ $tc('home.good', 1) }}
-                    span.text-warning(place="indifferent") {{ $tc('home.indifferent', 1) }}
-                    span.text-danger(place="bad") {{ $tc('home.bad', 1) }}
-                li.list-group-item.text-muted
-                  em {{ $t('home.step2.part2') }}
-
-          .card
-            .card-header
-              i18n.card-title.text-muted(path="home.step3.title" tag="h6")
-                span.h5.text-primary(place="step") {{ $t('home.step', {step: 3})}}
-            .card-body
-              ul.list-group.list-group-flush
-                li.list-group-item.text-muted
-                  em {{ $t('home.step3.part1') }}
-                li.list-group-item
-                  img.img-fluid.w-75.border.border-success.rounded(src="../assets/event-overview-calendar.png")
-                li.list-group-item.text-muted
-                  i18n(path="home.step3.part2" tag="em")
-                    span.text-success(place="good") {{ $tc('home.good_upper', 2) }}
-                    span.text-danger(place="bad") {{ $tc('home.bad', 2) }}
-                li.list-group-item.text-muted
-                  em {{ $t('home.step3.part3') }}
-
-      .card-footer
-        .row.justify-content-center
-          .col-12.col-md-auto
-            router-link.btn.btn-block.btn-success(
-              role="button"
-              :to="{ name: 'new_event'}"
-            )
-              i.fas.fa-plus
-              | &nbsp; {{ $t('home.create_new_event') }}
-
+    div
+        b-card
+            b-card-title.text-center
+                h1.display-3.text-success
+                    em {{ $t('app_name') }}
+            b-card-sub-title.text-center
+                h1.text-muted {{ $t('home.title') }}
+            b-card-text.text-center.m-5
+                router-link.btn.btn-lg.btn-success(
+                    role="button"
+                    :to="{ name: 'new_event'}"
+                )
+                    i.fas.fa-handshake
+                    | &nbsp; {{ $t('home.create_new_event') }}
+            b-card-text.text-center
+                h1 {{ $t('home.how_it_works') }}
+            b-card-group(deck)
+                b-card.text-center
+                    b-card-title
+                        h2 {{ $t('home.step1.title') }}
+                    b-card-text(text-tag="h5")
+                        em.text-muted {{ $t('home.step1.part1') }}
+                    b-card-text(text-tag="h5")
+                        em.text-muted {{ $t('home.step1.part2') }}
+                    img.img-fluid.w-75.border.border-primary.rounded(src="../assets/event-creation-calendar.png")
+                b-card.text-center
+                    b-card-title
+                        h2 {{ $t('home.step2.title') }}
+                    img.img-fluid.border.border-primary.rounded(src="../assets/single-day-details.png")
+                    b-card-text(text-tag="h5")
+                        i18n.text-muted(path="home.step2.part1" tag="em")
+                            span.text-success(place="good") {{ $tc('home.good', 1) }}
+                            span.text-warning(place="indifferent") {{ $tc('home.indifferent', 1) }}
+                            span.text-danger(place="bad") {{ $tc('home.bad', 1) }}
+                b-card.text-center
+                    b-card-title
+                        h2 {{ $t('home.step3.title') }}
+                    b-card-text(text-tag="h5")
+                        em.text-muted {{ $t('home.step3.part1') }}
+                    img.img-fluid.border.border-success.rounded(src="../assets/event-overview-calendar.png")
+                    b-card-text(text-tag="h5")
+                        i18n.text-muted(path="home.step3.part2" tag="em")
+                            span.text-success(place="good") {{ $tc('home.good_upper', 2) }}
+                            span.text-danger(place="bad") {{ $tc('home.bad', 2) }}
+                    b-card-text(text-tag="h5")
+                        em.text-muted {{ $t('home.step3.part3') }}
 </template>
+<style>
+.card-text, .card-title {
+    padding-bottom: 10px;
+    padding-top: 10px;
+}
+img {
+    margin-bottom: 10px;
+    margin-top: 10px;
+}
+</style>
