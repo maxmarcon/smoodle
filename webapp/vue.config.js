6fd454c8abdd9fafb3747c7753c28483fc41244f
Max Marcon <maxmarcon@gmx.net>
2020-02-11 23:31:26 +0100

    fixed tests

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -8,4 +8,10 @@ module.exports = {
     }
   },
   outputDir: '../priv/static'
+  // v-calendar includes ES6 code:
+  // https://github.com/nathanreyes/v-calendar/issues/513
+  // this causes issues in IE11 and Edge
+  // the line below should fix the problem but it doesn't fully,
+  // the code loads but the calendar is not visualized correctly
+  // transpileDependencies: ['v-calendar']
 }
