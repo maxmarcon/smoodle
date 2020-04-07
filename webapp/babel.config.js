2fb481296754a205162e0f24115e54f31205bdce
Max Marcon <maxmarcon@gmx.net>
2020-04-07 09:16:38 +0200

    removed unnecessary polyfill

--- webapp/babel.config.js
+++ webapp/babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  // TODO: can this be removed or does Edge still need it?
-    presets: [
-        ['@vue/app', {
-            polyfills: [
-                'es.array.includes'
-            ]
-        }]
-    ]
+  presets: [
+    '@vue/app'
+  ]
 }
