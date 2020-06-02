b58e4e714b2936d2fb18bcbdbe145cf60a12c286
Max Marcon <maxmarcon@gmx.net>
2020-06-02 17:42:21 +0200

    testing config reader for releases in production

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,11 @@ import Config
 
 config :smoodle, SmoodleWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [host: System.get_env("HOST_NAME"), scheme: "http", port: System.get_env("PORT")],
+  url: [
+    host: System.get_env("HOST_NAME"),
+    scheme: System.get_env("SCHEME", "https"),
+    port: System.get_env("PORT", "443")
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE}"),
   server: true
@@ -14,3 +18,12 @@ config :smoodle, Smoodle.Repo,
   url: System.get_env("DATABASE_URL"),
   ssl: false,
   pool_size: 2
+
+if System.get_env("LOCAL_EMAILS") do
+  config :smoodle, Smoodle.Mailer, adapter: Bamboo.LocalAdapter
+else
+  config :smoodle, Smoodle.Mailer,
+    adapter: Bamboo.MailjetAdapter,
+    api_key: System.get_env("MAILJET_API_KEY"),
+    api_private_key: System.get_env("MAILJET_API_PRIVATE_KEY")
+end
