37c02ab6382021805d6e5f5a1e8551b382e9bc34
Max Marcon <maxmarcon@gmx.net>
2020-08-02 16:32:33 +0200

    added script to update both mix and npm versions at once

---
+++ update_version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+MIX_VERSION=$(mix version.current)
+NPM_VERSION=$(cd webapp && jq --raw-output  .version package.json)
+
+if [ $MIX_VERSION != $NPM_VERSION ]; then
+  echo "Current version mismatch: MIX_VERSION=${MIX_VERSION}, but NPM_VERSION=${NPM_VERSION}"
+  exit 1
+fi
+
+(cd webapp && yarn version --no-git-tag-version --non-interactive --$1) \
+&& git add webapp/package.json \
+&& mix version.up $1 \
+&& mix version.tag
