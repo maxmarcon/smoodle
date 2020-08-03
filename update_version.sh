ef285b2aadd3cb3f0c0fe7db0caee10ecc3fb656
Max Marcon <maxmarcon@gmx.net>
2020-08-03 21:57:31 +0200

    made update_version.sh more user-friendly

--- update_version.sh
+++ update_version.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
+case $1 in
+  major|minor|patch)
+  ;;
+  *)
+  echo "Usage: $0 <major|minor|patch>"
+  exit 1
+esac
+
 MIX_VERSION=$(mix version.current)
-NPM_VERSION=$(cd webapp && jq --raw-output  .version package.json)
+NPM_VERSION=$(cd webapp && jq --raw-output .version package.json)
 
 if [ $MIX_VERSION != $NPM_VERSION ]; then
   echo "Current version mismatch: MIX_VERSION=${MIX_VERSION}, but NPM_VERSION=${NPM_VERSION}"
   exit 1
 fi
 
-(cd webapp && yarn version --no-git-tag-version --non-interactive --$1) \
-&& git add webapp/package.json \
-&& mix version.up $1 \
-&& mix version.tag
+(cd webapp && yarn version --no-git-tag-version --non-interactive --$1) &&
+  git add webapp/package.json &&
+  mix version.up $1 &&
+  mix version.tag
