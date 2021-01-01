c0ec831757b6e70d53d8c21ad6bcbf518f3e4aca
Max Marcon <maxmarcon@gmx.net>
2021-01-01 15:04:12 +0100

    made update_version.sh fool-proof

--- update_version.sh
+++ update_version.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
 case $1 in
-  major|minor|patch)
-  ;;
-  *)
+major | minor | patch) ;;
+
+*)
   echo "Usage: $0 <major|minor|patch>"
   exit 1
+  ;;
 esac
 
-MIX_VERSION=$(mix version.current)
-NPM_VERSION=$(cd webapp && jq --raw-output .version package.json)
+check_version_consistency() {
+  MIX_VERSION=$(mix version.current)
+  NPM_VERSION=$(cd webapp && jq --raw-output .version package.json)
 
-if [ $MIX_VERSION != $NPM_VERSION ]; then
-  echo "Current version mismatch: MIX_VERSION=${MIX_VERSION}, but NPM_VERSION=${NPM_VERSION}"
-  exit 1
-fi
+  if [ $MIX_VERSION != $NPM_VERSION ]; then
+    echo "Current version mismatch: MIX_VERSION=${MIX_VERSION}, but NPM_VERSION=${NPM_VERSION}"
+    exit 1
+  fi
+}
+
+check_version_consistency
 
 (cd webapp && yarn version --no-git-tag-version --non-interactive --$1) &&
   git add webapp/package.json &&
   mix version.up $1 &&
+  check_version_consistency &&
   mix version.tag
