ceff746615a35b1511972aacb8525ba966f08384
Max Marcon <maxmarcon@gmx.net>
2021-04-01 23:20:07 +0200

    v1.9.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smoodle.Mixfile do
   def project do
     [
       app: :smoodle,
-      version: "1.9.3",
+      version: "1.9.4",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
